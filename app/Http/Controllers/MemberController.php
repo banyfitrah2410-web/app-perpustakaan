c5b24bbff0c91707a58380d1d3d1883d45063a64
banyfitrah2410-web <banyfitrah2410@gmail.com>
2026-09-17 20:46:03 +0700

    [P-4] tambah master layout blade dan view index semua resource

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -8,9 +8,9 @@ use Illuminate\Http\Request;
 class MemberController extends Controller
 {
     private array $members = [
-        ['id' => 1, 'nama' => 'Bany', 'nrp' => '3125600108', 'email' => 'Bany@student.pens.ac.id', 'nomor_telepon' => '081234567890', 'alamat' => 'Jl. Raya ITS, Sukolilo, Surabaya', 'status' => 'aktif'],
-        ['id' => 2, 'nama' => 'Hawa', 'nrp' => '3125600109', 'email' => 'Hawa@student.pens.ac.id', 'nomor_telepon' => '082198765432', 'alamat' => 'Jl. Keputih Tegal, Surabaya', 'status' => 'aktif'],
-        ['id' => 3, 'nama' => 'Abi', 'nrp' => '3125600110', 'email' => 'Abi@student.pens.ac.id', 'nomor_telepon' => null, 'alamat' => 'Jl. Gebang Putih, Surabaya', 'status' => 'nonaktif'],
+        ['id' => 1, 'nama' => 'Siti Aminah', 'nim' => '2310501001', 'email' => 'siti.aminah@pens.ac.id', 'nomor_telepon' => '081234567890', 'status' => 'aktif'],
+        ['id' => 2, 'nama' => 'Budi Santoso', 'nim' => '2310501002', 'email' => 'budi.santoso@pens.ac.id', 'nomor_telepon' => '081298765432', 'status' => 'aktif'],
+        ['id' => 3, 'nama' => 'Dewi Lestari', 'nim' => '2310501003', 'email' => 'dewi.lestari@pens.ac.id', 'nomor_telepon' => '081211122233', 'status' => 'nonaktif'],
     ];
 
     public function index()
